ac797a28f4db0495f314871d995ad0a7588e8de3
nbb2025 <nbb2025@qq.com>
2025-11-04 21:01:10 +0800

    feat: add template helpers for ssl policies

--- pkg/templates/edge.go
+++ pkg/templates/edge.go
@@ -5,6 +5,7 @@ import (
 	_ "embed"
 	"fmt"
 	"os"
+	"strings"
 	"text/template"
 	"time"
 )
@@ -13,7 +14,11 @@ import (
 var edgeTemplate string
 
 var edgeFuncMap = template.FuncMap{
-	"now": func() time.Time { return time.Now().UTC() },
+	"now":            func() time.Time { return time.Now().UTC() },
+	"sslPolicyFor":   sslPolicyFor,
+	"certificateFor": certificateFor,
+	"hstsSeconds":    hstsSeconds,
+	"tlsProtocols":   tlsProtocols,
 }
 
 // RenderEdge writes the rendered HTTP config to disk.
@@ -32,4 +37,43 @@ func RenderEdge(data EdgeTemplateData, outputPath string, templatePath string) e
 		return fmt.Errorf("write output: %w", err)
 	}
 	return nil
+}
+
+func certificateFor(certs map[string]CertificateMaterial, accountID string) *CertificateMaterial {
+	if cert, ok := certs[accountID]; ok {
+		return &cert
+	}
+	return nil
+}
+
+func sslPolicyFor(policies []SSLPolicy, accountID string) *SSLPolicy {
+	for _, policy := range policies {
+		if policy.Scope.AppliesToDomain(accountID) {
+			return &policy
+		}
+	}
+	return nil
+}
+
+func hstsSeconds(d time.Duration) int64 {
+	if d <= 0 {
+		return int64((24 * time.Hour).Seconds())
+	}
+	return int64(d.Seconds())
+}
+
+func tlsProtocols(minVersion string) string {
+	min := strings.TrimSpace(strings.ToLower(minVersion))
+	switch min {
+	case "tls1.3", "tls13":
+		return "TLSv1.3"
+	case "tls1.2", "tls12":
+		return "TLSv1.2 TLSv1.3"
+	case "tls1.1", "tls11":
+		return "TLSv1.1 TLSv1.2 TLSv1.3"
+	case "tls1.0", "tls10":
+		return "TLSv1 TLSv1.1 TLSv1.2 TLSv1.3"
+	default:
+		return "TLSv1.2 TLSv1.3"
+	}
 }
